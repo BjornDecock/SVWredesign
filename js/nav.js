9e26d92d0deec1950e7632d990f1dab5acbc296d
Bjorn Decock <bdecock@telenet.be>
2018-10-09 11:49:08 +0200

    Mobiele versie

---
+++ js/nav.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("mobile-header").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mobile-header").style.height = "0%";
+}
